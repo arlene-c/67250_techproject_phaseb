8f57ec12aeef73eee279b503d656fd00a124d57c
arlene-c <137818717+arlene-c@users.noreply.github.com>
2026-04-04 02:03:28 -0400

    Add files via upload

---
+++ arlenec-increment4/static/script.js
@@ -0,0 +1,77 @@
+
+const now = new Date();
+const hours = now.getHours();
+function greeting(x) {
+    var page = window.location.pathname;
+    const greetingElement = document.getElementById("greeting");
+    if (page.endsWith("index.html") || page === "/") {
+        if (greetingElement) {
+            if (x < 5 || x >= 20) {
+                greetingElement.innerHTML = "Good night";
+            } else if (x < 12) {
+                greetingElement.innerHTML = "Good morning";
+            } else if (x < 18) {
+                greetingElement.innerHTML = "Good afternoon";
+            } else {
+                greetingElement.innerHTML = "Good evening";
+            }
+        }
+    }
+}
+greeting(hours);
+
+
+function addYear() {
+    var currentYear = new Date().getFullYear();
+    var year = document.getElementById("copyYear");
+    if (year) {
+        year.innerHTML = currentYear;
+    }
+}
+
+
+
+function ActiveNav() {
+    // Get all navigation links
+    const navLinks = document.querySelectorAll('nav a');
+
+    // Iterate over each link
+    navLinks.forEach(link => {
+    // Check if the link's href matches the current window location
+        if (window.location.href === link.href) {
+            // Add the 'active' class to highlight the current page
+            link.classList.add("active");
+        }
+    });
+}
+
+ActiveNav();
+
+
+ $("#readLess").click(function(){ 
+    $("#longIntro").hide(); 
+    $("#readLess").hide();  
+    $("#readMore").show();  
+
+  });
+
+  $("#readMore").click(function(){
+    $("#longIntro").show();  
+    $("#readLess").show();   
+    $("#readMore").hide();   
+  });
+
+function revealForm() {
+    var formSection = document.getElementById("checkoutSection");
+    if (formSection) {
+        formSection.style.display = "block";
+    }
+}
+
+function submitPurchase() {
+    alert("Redirecting to payment system.");
+}
+
+
+
+
